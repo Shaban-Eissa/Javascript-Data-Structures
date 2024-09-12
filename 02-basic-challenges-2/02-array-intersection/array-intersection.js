d4cf6819d04b59d2609f371f81606f2f0114bd71
Shaban-Eissa <shabaneissa767@gmail.com>
2024-09-12 14:18:36 +0300

    Refactor arrayIntersection function to correctly find the intersection of two arrays

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,13 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  let set1 = new Set(arr1);
+  let intersection = [];
+
+  for (let num of arr2) {
+    if (set1.has(num)) {
+      intersection.push(num);
+    }
+  }
+  return intersection;
+}
 
 module.exports = arrayIntersection;
