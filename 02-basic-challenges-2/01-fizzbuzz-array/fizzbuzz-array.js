66777eb7a9d45755c30535e4e6ffa0cd37ef782b
Shaban-Eissa <shabaneissa767@gmail.com>
2024-09-12 14:07:36 +0300

    Refactor fizzBuzzArray function to correctly generate a FizzBuzz array

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,18 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+  let result = [];
+
+  for (let item = 1; item <= num; item++) {
+    if (item % 3 === 0 && item % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (item % 3 === 0) {
+      result.push("Fizz");
+    } else if (item % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(item);
+    }
+  }
+  return result;
+}
 
 module.exports = fizzBuzzArray;
