73064304a59fb570895ad1e4c63b3820167fdddf
Shaban-Eissa <shabaneissa767@gmail.com>
2024-09-12 13:34:47 +0300

    Refactor titleCase function to correctly convert a string to title case

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,11 @@
-function titleCase() {}
+function titleCase(str) {
+  const words = str.toLowerCase().split(" ");
+
+  words.map((word, index) => {
+    words[index] = words[index][0].toUpperCase() + words[index].slice(1);
+  });
+
+  return words.join(" ");
+}
 
 module.exports = titleCase;
