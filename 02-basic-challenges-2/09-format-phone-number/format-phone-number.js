dbfc27348e3224392f2ee7646b16c41fa840070f
Shaban-Eissa <shabaneissa767@gmail.com>
2024-09-12 14:52:13 +0300

    Refactor formatPhoneNumber function to correctly format a phone number

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,6 @@
-function formatPhoneNumber() {}
+const formatPhoneNumber = (numbers) =>
+  `(${numbers.slice(0, 3).join("")}) ${numbers.slice(3, 6).join("")}-${numbers
+    .slice(6)
+    .join("")}`;
 
 module.exports = formatPhoneNumber;
