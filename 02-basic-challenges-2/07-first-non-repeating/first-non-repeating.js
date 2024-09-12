e951e143550c4423ef78e00c00f8400c7406e7c0
Shaban-Eissa <shabaneissa767@gmail.com>
2024-09-12 14:48:35 +0300

    Refactor findFirstNonRepeatingCharacter function to correctly find the first non-repeating character in a string

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,18 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  const charCount = {};
+
+  // Count the occurrences of each character
+  for (const char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  // Find the first non-repeating character
+  for (const char of str) {
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
