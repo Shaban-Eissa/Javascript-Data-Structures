66789f9625db4884139d44a854640761ab8e2863
Shaban-Eissa <shabaneissa767@gmail.com>
2024-09-29 17:15:56 +0300

    💥 NEW: Adding logic for using filter & map & reduce for the same array of items

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,8 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+  return numbers
+    .filter((num) => num % 2 === 0)
+    .map((num) => num ** 2)
+    .reduce((total, num) => total + num, 0);
+}
 
 module.exports = sumOfEvenSquares;
