4e1e25f0e867add141bf75878260e8b3d3ed98fc
Shaban-Eissa <shabaneissa767@gmail.com>
2024-09-29 17:28:43 +0300

    💥 NEW: Using reduce function

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,12 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+  const totalWithoutTaxes = products.reduce(
+    (sum, product) => sum + product.price * product.quantity,
+    0
+  );
+
+  const taxAmount = (totalWithoutTaxes * taxRate) / 100;
+  const totalWithTaxes = totalWithoutTaxes + taxAmount;
+  return parseFloat(totalWithTaxes.toFixed(2));
+}
 
 module.exports = calculateTotalSalesWithTax;
