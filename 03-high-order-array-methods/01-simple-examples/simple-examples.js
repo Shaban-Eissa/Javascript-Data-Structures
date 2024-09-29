ec3a5e974944c1120cf09e4bf59b6f5a74f825f4
Shaban-Eissa <shabaneissa767@gmail.com>
2024-09-29 16:59:55 +0300

    Refactor high-order array methods to correctly transform, filter, reduce, iterate, find, check, and apply functions on an array

--- 03-high-order-array-methods/01-simple-examples/simple-examples.js
+++ 03-high-order-array-methods/01-simple-examples/simple-examples.js
@@ -4,38 +4,41 @@ const numbers = [1, 2, 3, 4, 5];
  * map: Transforms array elements with a provided function, creating a new array.
  */
 
+const doubleNums = numbers.map((num) => num * 2);
 
 /**
  * filter: Creates a new array with elements that satisfy a specified condition.
  */
 
-
+const evenNumbers = numbers.filter((num) => num % 2 === 0);
 
 /**
  * reduce: Accumulates array elements into a single value using a provided function.
  */
 
-
+const totalOfNumbers = numbers.reduce((total, num) => {
+  return total + num;
+}, 0);
 
 /**
  * forEach: Iterates through array elements and applies a function without creating a new array.
  */
 
+numbers.forEach((num) => console.log(num));
 
-
- /**
+/**
  * find: Returns the first array element that satisfies a specified condition.
  */
 
-
+const foundNumber = numbers.find((num) => num > 2);
 /**
  * some: Checks if at least one array element satisfies a condition.
  */
 
- 
+const hasEvenNumber = numbers.some((num) => num > 5);
 
 /**
  * every: Checks if all array elements satisfy a condition.
  */
 
-
+const allNumsGreaterThanOne = numbers.every((num) => num > 1);
