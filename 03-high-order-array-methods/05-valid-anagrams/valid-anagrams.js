39d82a97ed273db32113788708b2a7961541939b
Shaban-Eissa <shabaneissa767@gmail.com>
2024-09-29 17:45:17 +0300

    💥 NEW: Using frequencyArray with reduce()

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,17 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+  const freqCountForStr1 = str1.split("").reduce((acc, char) => {
+    acc[char] = (acc[char] || 0) + 1;
+    return acc;
+  }, {});
+
+  const freqCountForStr2 = str2.split("").reduce((acc, char) => {
+    acc[char] = (acc[char] || 0) + 1;
+    return acc;
+  }, {});
+
+  return Object.keys(freqCountForStr1).every(
+    (char) => freqCountForStr1[char] === freqCountForStr2[char]
+  );
+}
 
 module.exports = validAnagrams;
