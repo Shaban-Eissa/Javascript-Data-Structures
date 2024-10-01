a773369931ba81b7b05eefc9772ee80ef2e9bed6
Shaban-Eissa <shabaneissa767@gmail.com>
2024-10-01 19:38:11 +0300

    💥 NEW: Building a function to reverse string with recursion

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,7 @@
-function reverseString() {}
-
+function reverseString(str) {
+  if (str === "") return "";
+  else {
+    return reverseString(str.substr(1)) + str.charAt(0);
+  }
+}
 module.exports = reverseString;
