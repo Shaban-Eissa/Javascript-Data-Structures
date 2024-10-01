14fe382577e2d42320533655edc72a1fd9e6b4ea
Shaban-Eissa <shabaneissa767@gmail.com>
2024-10-01 20:36:28 +0300

    💥 NEW: Building a function that make sum for array items in Time O(n^2)

--- 05-complexity/05-quadratic-time-complexity/quadradic-time.js
+++ 05-complexity/05-quadratic-time-complexity/quadradic-time.js
@@ -3,3 +3,15 @@ Quadradic Time O(n^2)
 
 Quadradic time means that the time required to complete a function is proportional to the square of the input data set.
 */
+
+function sumArray(arr) {
+    let sum = 0;
+    let sum2 = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+      for (let j = 0; j < arr.length; j++) {
+        sum2 += arr[j];
+      }
+    }
+    return sum + sum2;
+  }
