18d24269021662b2d7ae7edb7b24c49df62248c1
Shaban-Eissa <shabaneissa767@gmail.com>
2024-10-01 20:03:10 +0300

    💥 NEW: Building flatten-array function with recursion

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,12 @@
-function flattenArray() {}
+function flattenArray(arr) {
+  let result = [];
+  for (let item of arr) {
+    if (Array.isArray(item)) {
+      result = result.concat(flattenArray(item));
+    } else {
+      result.push(item);
+    }
+  }
+}
 
 module.exports = flattenArray;
