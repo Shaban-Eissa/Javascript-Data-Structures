03914f2d37315ef7485f3be17f6e525400da4a7c
Shaban-Eissa <shabaneissa767@gmail.com>
2024-10-01 19:59:40 +0300

    💥 NEW: Building rangeOfNumbers function with recursion

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,13 @@
-function numberRange() {}
+function rangeOfNumbers(startNum, endNum) {
+  if (startNum === endNum) {
+    return [startNum];
+  }
+
+  const numbers = rangeOfNumbers(startNum, endNum - 1);
+
+  numbers.push(endNum);
+
+  return numbers;
+}
 
 module.exports = numberRange;
