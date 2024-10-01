b4d02c9ff38a4869914ff05d99c29bd7765a98dc
Shaban-Eissa <shabaneissa767@gmail.com>
2024-10-01 17:04:31 +0300

    Refactor password-validator to improve password validation logic

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,19 @@
-function validatePassword() {}
+function validatePassword(password) {
+  const passwordLength = password.length >= 8;
+
+  const hasUppercase = password
+    .split("")
+    .some((char) => char === char.toUpperCase() && char !== char.toLowerCase());
+
+  const hasLowercase = password
+    .split("")
+    .some((char) => char === char.toLowerCase() && char !== char.toUpperCase());
+
+  const hasDigits = password
+    .split("")
+    .some((char) => !isNaN(parseInt(char, 10)));
+
+  return passwordLength && hasUppercase && hasLowercase && hasDigits;
+}
 
 module.exports = validatePassword;
