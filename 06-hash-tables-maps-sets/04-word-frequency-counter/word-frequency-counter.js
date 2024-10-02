86f172275f7f4675b214e242bdfce210ba446ec5
Shaban-Eissa <shabaneissa767@gmail.com>
2024-10-02 09:48:43 +0300

    💥 NEW: Building wordFrequencyCounter() by using Map() to find the frequency for each word in the string

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,3 +1,18 @@
-function wordFrequencyCounter() {}
+function wordFrequencyCounter(str) {
+  let words = str.toLowerCase().split(/\W+/);
+
+  const wordFrequency = new Map();
+
+  for (let word of words) {
+    if (word === "") continue;
+
+    if (wordFrequency.has(word)) {
+      wordFrequency.set(word, wordFrequency.get(word) + 1);
+    } else {
+      wordFrequency.set(word, 1);
+    }
+  }
+  return wordFrequency;
+}
 
 module.exports = wordFrequencyCounter;
