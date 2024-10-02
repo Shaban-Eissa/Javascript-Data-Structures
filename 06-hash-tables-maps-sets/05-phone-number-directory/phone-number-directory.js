e66b243c0b109bedd88e09104707f6185d35e262
Shaban-Eissa <shabaneissa767@gmail.com>
2024-10-02 09:54:10 +0300

    💥 NEW: Building phoneNumberDirectory() by using Map() to find name and number

--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,13 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(phoneNumbers) {
+  let directory = new Map();
+
+  for (let entry of phoneNumbers) {
+    const [name, number] = entry.split(":");
+
+    directory.set(name, number);
+  }
+
+  return directory;
+}
 
 module.exports = phoneNumberDirectory;
