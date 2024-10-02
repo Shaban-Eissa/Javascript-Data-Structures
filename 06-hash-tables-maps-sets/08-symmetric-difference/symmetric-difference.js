7cf14bc9c8974891c7938b8f9b2004b05eaada81
Shaban-Eissa <shabaneissa767@gmail.com>
2024-10-02 10:18:10 +0300

    💥 NEW: Building symmetricDifference() by using Set()

--- 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
+++ 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
@@ -1,3 +1,22 @@
-function symmetricDifference() {}
+function symmetricDifference(arr1, arr2) {
+  let Set1 = new Set(arr1);
+  let Set2 = new Set(arr2);
+
+  let result = [];
+
+  for (let num of arr1) {
+    if (!Set2.has(num)) {
+      result.push(num);
+    }
+  }
+
+  for (const num of arr2) {
+    if (!Set1.has(num)) {
+      result.push(num);
+    }
+  }
+
+  return result;
+}
 
 module.exports = symmetricDifference;
