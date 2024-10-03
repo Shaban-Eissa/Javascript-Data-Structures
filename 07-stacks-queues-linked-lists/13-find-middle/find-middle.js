eeb88bf97238d47222ba4f5b9c007b0236d89db0
Shaban-Eissa <shabaneissa767@gmail.com>
2024-10-03 11:50:17 +0300

    💥 NEW: Building findMiddle() with LinkedList

--- 07-stacks-queues-linked-lists/13-find-middle/find-middle.js
+++ 07-stacks-queues-linked-lists/13-find-middle/find-middle.js
@@ -1,3 +1,23 @@
-function findMiddle() {}
+function findMiddle(list) {
+  let slow = list.head;
+  let fast = list.head;
+  let prev = null;
+
+  while (fast !== null && fast.next !== null) {
+    // Move fast forward by 2 nodes
+    fast = fast.next.next;
+    // Move slow forward by 1 node
+    prev = slow;
+    slow = slow.next;
+  }
+
+  if (fast === null) {
+    // Even number of nodes
+    return prev.next;
+  } else {
+    // Odd number of nodes
+    return slow;
+  }
+}
 
 module.exports = findMiddle;
