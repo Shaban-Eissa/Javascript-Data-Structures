9323c7b1c1d631d8e79a6a0a8d9b422e98c6e570
Shaban-Eissa <shabaneissa767@gmail.com>
2024-10-03 10:40:57 +0300

    💥 NEW: Building reverseStringWithQueue() by using operations like enqueue & dequeue

--- 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
+++ 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
@@ -1,5 +1,19 @@
-const Queue = require('./queue');
+const Queue = require("./queue");
 
-const reverseStringWithQueue = (str) => {};
+const reverseStringWithQueue = (str) => {
+  let queue = new Queue();
+
+  for (let i = 0; i < str.length; i++) {
+    queue.enqueue(str[i]);
+  }
+
+  let reversedString = "";
+
+  while (!queue.isEmpty()) {
+    reversedString += queue.dequeue();
+  }
+
+  return reversedString;
+};
 
 module.exports = reverseStringWithQueue;
