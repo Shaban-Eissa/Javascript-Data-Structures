f4f4a03d3c8102e062e8e61156a43fc40cfc6b06
Shaban-Eissa <shabaneissa767@gmail.com>
2024-10-03 09:04:33 +0300

    Refactor stack implementation
    This commit refactors the stack implementation and its corresponding tests. The Stack class now includes methods for checking if the stack is full or empty, pushing elements onto the stack, popping elements from the stack, and peeking at the top element without removing it. The tests have been updated to reflect these changes.

--- 07-stacks-queues-linked-lists/02-stack-implementation/stack.js
+++ 07-stacks-queues-linked-lists/02-stack-implementation/stack.js
@@ -1,3 +1,43 @@
-class Stack {}
+class Stack {
+  constructor() {
+    this.maxSize = 100;
+    this.stack = [];
+    this.top = -1;
+  }
+
+  isFull() {
+    return this.top === this.maxSize - 1;
+  }
+
+  isEmpty() {
+    return this.top === -1;
+  }
+
+  push(value) {
+    if (this.isFull()) {
+      return false;
+    }
+
+    this.top += 1;
+    this.stack[this.top] = value;
+    return true;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    this.top -= 1;
+    return this.stack.pop();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    return this.stack[this.top];
+  }
+}
 
 module.exports = Stack;
