659e283649edfae2f0b5418d26fd49f001bf46a6
Shaban-Eissa <shabaneissa767@gmail.com>
2024-10-04 09:14:07 +0300

    💥 NEW: Building Graph (DFS)

--- 08-binary-trees-graphs/14-graph-depth-first-traversal/graph.js
+++ 08-binary-trees-graphs/14-graph-depth-first-traversal/graph.js
@@ -1,41 +1,29 @@
-class Graph {
-  constructor() {
-    this.adjacencyList = {};
+const Stack = require("./stack");
+
+function depthFirstTraversal(graph, startingVertex) {
+  if (!graph.adjacencyList[startingVertex]) {
+    return [];
   }
 
-  addVertex(vertex) {
-    this.adjacencyList[vertex] = [];
+  const visited = {};
+  const stack = new Stack();
+  const result = [];
+  stack.push(startingVertex);
+  visited[startingVertex] = true;
+
+  while (!stack.isEmpty()) {
+    const currentVertex = stack.pop();
+    result.push(currentVertex);
+
+    graph.adjacencyList[currentVertex].forEach((neighbor) => {
+      if (!visited[neighbor]) {
+        visited[neighbor] = true;
+        stack.push(neighbor);
+      }
+    });
   }
 
-  addEdge(vertex1, vertex2) {
-    this.adjacencyList[vertex1].push(vertex2);
-    this.adjacencyList[vertex2].push(vertex1);
-  }
-
-  removeEdge(vertex1, vertex2) {
-    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
-      (v) => v !== vertex2
-    );
-
-    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
-      (v) => v !== vertex1
-    );
-  }
-
-  removeVertex(vertex) {
-    while (this.adjacencyList[vertex].length) {
-      const adjacentVertex = this.adjacencyList[vertex].pop();
-      this.removeEdge(vertex, adjacentVertex);
-    }
-    delete this.adjacencyList[vertex];
-  }
-
-  printAdjacencyList() {
-    for (const vertex in this.adjacencyList) {
-      console.log(`${vertex} -> ${this.adjacencyList[vertex].join(', ')}`);
-    }
-  }
+  return result;
 }
 
-
-module.exports = Graph;
+module.exports = depthFirstTraversal;
